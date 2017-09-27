3eccc56c732949c3b6f3b2a7dc514f3521924d34
KathleenRepko <kathleen.repko@gmail.com>
2017-09-27 19:42:24 -0400

    Completed #10-12.

--- HW_0927_Operators/Program.cs
+++ HW_0927_Operators/Program.cs
@@ -10,6 +10,41 @@ namespace HW_0927_Operators
     {
         static void Main(string[] args)
         {
+            // #1-9 were completed successfully, then lost during an unfortunate renaming tragedy.
+
+            // #10
+            // The sum of three consecutive numbers is 72. What are the smallest of these numbers?
+            //int sumNumber = 72;
+            //int firstNumber = (sumNumber - 3) / 3;
+            //Console.WriteLine("The smallest number is " + firstNumber + ".");
+
+            // #11
+            // The sum of three consecutive even numbers is 48. What are the smallest of these numbers?
+            //int sumNumber = 48;
+            //int firstNumber = (sumNumber - 3) / 3;
+            //Console.WriteLine("The smallest number is " + firstNumber + ".");
+
+            // #12
+            // Maria bought seven boxes. A week later half of all her boxes were destroyed in a fire. 
+            // There are now only 22 boxes left. With how many did she start?
+            int currentNumber = 22;
+            int preFire = currentNumber * 2;
+            int boxPurchase = 7;
+            int startNumber = preFire - boxPurchase;
+            Console.WriteLine("Maria started with " + startNumber + " boxes.");
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
